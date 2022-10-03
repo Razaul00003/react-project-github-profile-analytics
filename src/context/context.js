d7e9230707feb27c5d4eb890a1436f720e8446be
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-10-03 23:01:49 +0600

    second commit

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
+
+const GithubContext = React.createContext();
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setrepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+  //requesting data using api
+  const [requests, setRequests] = useState(0);
+  const [loading, setLoading] = useState(false);
+  //check search limit
+  const checkRequestLimit = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          //throw errro
+        }
+      })
+      .catch((err) => {});
+  };
+  //error
+  useEffect(checkRequestLimit, []);
+  return (
+    <GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
+      {children}
+    </GithubContext.Provider>
+  );
+};
+export { GithubContext, GithubProvider };
